f6992a72c61e69609c18491580d878d3136934ed
maukas <maukasgrain@gmail.com>
2019-08-13 09:40:36 -0600

    Add DataController ( anonymous path )

---
+++ TheRedPillAPI/Controllers/DataController.cs
@@ -0,0 +1,30 @@
+﻿using DataModels.DataModels;
+using Microsoft.AspNetCore.Mvc;
+using Services.IServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheRedPillAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DataController : Controller
+    {
+        public DataController (IDataService dataservice )
+        {
+            _dataservice = dataservice;
+        }
+
+        private IDataService _dataservice;
+
+        [HttpPost("anonymousdata")]
+        public IActionResult PostAnonymousData(AnonymousSessionDataModel sessiondata)
+        {
+            _dataservice.AddAnonymousSessionData(sessiondata);
+            return Ok();
+        }
+    }
+
+}
